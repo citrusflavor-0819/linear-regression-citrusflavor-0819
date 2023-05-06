df677529fcf4ed5b9a79bc499ec87b33a3c63b1d
citrusflavor-0819 <131932766+citrusflavor-0819@users.noreply.github.com>
2023-05-06 10:33:03 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -1,5 +1,3 @@
-# 最终在main函数中传入一个维度为6的numpy数组，输出预测值
-
 import os
 
 try:
@@ -8,14 +6,9 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-
-     
- def model(x,y):
-   return np.dot(np.linalg.inv(np.dot(x.T,x)),np.dot(x.T,y))
-    
 def ridge(data):
     n_alphas = 200
-    alphas = np.logspace(-3,4, n_alphas) #对数等比数列
+    alphas = np.logspace(-3, 4, n_alphas)  
     clf = linear_model.Ridge(fit_intercept=True)
     coefs = []
     score = []
@@ -25,7 +18,7 @@ def ridge(data):
         clf.set_params(alpha=a)
         clf.fit(X_train, y_train)
         coefs.append(clf.coef_)
-        m = clf.score(X_test,y_test)
+        m = clf.score(X_test, y_test)
         n = clf.intercept_
         score.append(m)
         intercept.append(n)
@@ -36,42 +29,36 @@ def ridge(data):
                                             'Visceraweight', 'Shellweight'],
                                'weights': coefs[i]})
             print(fd)
-            print('intercept:{}\nalphas:{}\nmax(score):{}'.format(intercept[i],alphas[i],score[i]))
+            print('intercept:{}\nalphas:{}\nmax(score):{}'.format(intercept[i], alphas[i], score[i]))
     clf1 = linear_model.Ridge(fit_intercept=True)
     clf1.set_params(alpha=c)
     clf1.fit(X_train, y_train)
     y_pre = clf1.predict(X_test)
-    # 评价
-    # 均方根误差(Root Mean Squared Error, RMSE)
     sum_mean = 0
     for i in range(len(y_pre)):
         sum_mean += (y_pre[i] - y_test.values[i]) ** 2
     sum_erro = np.sqrt(sum_mean / len(y_pre))
     print('The value of RMSE in max(score):', sum_erro)
     print('***************************************************')
-    # 交叉验证
+    # 浜ゅ弶楠岃瘉
     clf2 = linear_model.RidgeCV(alphas=alphas)
     clf2.fit(X_train, y_train)
     fr = pd.DataFrame({'variable': ['Sex', 'Length', 'Diameter', 'Height', 'Wholeweight', 'Shuckedweight',
                                     'Visceraweight', 'Shellweight'],
                        'weights': clf2.coef_})
     print(fr)
-    print('intercept:{}\nalphas:{}'.format(clf2.intercept_,clf2.alpha_))
+    print('intercept:{}\nalphas:{}'.format(clf2.intercept_, clf2.alpha_))
     y_pred = clf2.predict(X_test)
-    # print(y_pred)  # 1254个样本的预测结果
-    # 评价
-    # 均方根误差(Root Mean Squared Error, RMSE)
-    sum_mean = 0
+        sum_mean = 0
     for i in range(len(y_pred)):
         sum_mean += (y_pred[i] - y_test.values[i]) ** 2
     sum_erro = np.sqrt(sum_mean / len(y_pred))
     print('the value of RMSE in cross validation:', sum_erro)
-    
-    pass
-    
+   pass
+
 def lasso(data):
     n_alphas = 200
-    alphas = np.logspace(-5, 0, n_alphas)  # 对数等比数列
+    alphas = np.logspace(-5, 0, n_alphas)  
     coefs = []
     score = []
     intercept = []
@@ -94,19 +81,16 @@ def lasso(data):
             print(fd)
             print('intercept:{}\nalphas:{}\nmax(score):{}'.format(intercept[i], alphas[i], score[i]))
     las1 = Lasso(normalize=True)
-    las1.set_params(alpha = c)
+    las1.set_params(alpha=c)
     las1.fit(X_train, y_train)
     y_pre = las1.predict(X_test)
-    # 评价
-    # 均方根误差(Root Mean Squared Error, RMSE)
-    sum_mean = 0
+        sum_mean = 0
     for i in range(len(y_pre)):
         sum_mean += (y_pre[i] - y_test.values[i]) ** 2
     sum_erro = np.sqrt(sum_mean / len(y_pre))
     print('The value of RMSE in max(score):', sum_erro)
     print('***************************************************')
-    # 交叉验证，找到模型最优的alphas值
-    lasso_cv = LassoCV(alphas=alphas, normalize=True, max_iter=1000, cv=None)
+        lasso_cv = LassoCV(alphas=alphas, normalize=True, max_iter=1000, cv=None)
     lasso_cv.fit(X_train, y_train)
     fr = pd.DataFrame({'variable': ['Sex', 'Length', 'Diameter', 'Height', 'Wholeweight', 'Shuckedweight',
                                     'Visceraweight', 'Shellweight'],
@@ -114,25 +98,24 @@ def lasso(data):
     print(fr)
     print('intercept:{}\nalphas:{}'.format(lasso_cv.intercept_, lasso_cv.alpha_))
     y_pred = lasso_cv.predict(X_test)
-    # print(y_pred)  # 1254个样本的预测结果
-    # 评价
-    # 均方根误差(Root Mean Squared Error, RMSE)
     sum_mean = 0
     for i in range(len(y_pred)):
         sum_mean += (y_pred[i] - y_test.values[i]) ** 2
     sum_erro = np.sqrt(sum_mean / 1254)
     print('The value of RMSE in cross validation:', sum_erro)
- 
-
     pass
+
+def main(data):
+    x,y=read_data()
+    weight = model(x,y)
+    return data @ weight
+
+def model(x, y):
+        return np.dot(np.linalg.inv(np.dot(x.T, x)), np.dot(x.T, y))
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
 
-def main(data):
-    x,y=read_data()
-    weight =model(x,y)
-    return data @ weight
-     pass
+
