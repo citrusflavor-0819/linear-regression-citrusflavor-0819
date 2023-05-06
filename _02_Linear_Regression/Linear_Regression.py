147a718ec857488f87f1d5791b1f67485162a204
citrusflavor-0819 <131932766+citrusflavor-0819@users.noreply.github.com>
2023-05-06 10:25:53 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,11 +8,7 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-def main(data):
-    x,y=read_data()
-    weight =model(x,y)
-    return data @ weight
-     pass
+
      
  def model(x,y):
    return np.dot(np.linalg.inv(np.dot(x.T,x)),np.dot(x.T,y))
@@ -134,3 +130,9 @@ def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+
+def main(data):
+    x,y=read_data()
+    weight =model(x,y)
+    return data @ weight
+     pass
