af2b028ce174ab445f3bee17f1de5e88ade65d6c
citrusflavor-0819 <131932766+citrusflavor-0819@users.noreply.github.com>
2023-05-06 20:38:16 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -6,104 +6,20 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-    n_alphas = 200
-    alphas = np.logspace(-3, 4, n_alphas)  
-    clf = linear_model.Ridge(fit_intercept=True)
-    coefs = []
-    score = []
-    intercept = []
-    c = 0
-    for a in alphas:
-        clf.set_params(alpha=a)
-        clf.fit(X_train, y_train)
-        coefs.append(clf.coef_)
-        m = clf.score(X_test, y_test)
-        n = clf.intercept_
-        score.append(m)
-        intercept.append(n)
-    for i in range(len(score)):
-        if score[i] == max(score):
-            c = alphas[i]
-            fd = pd.DataFrame({'variable': ['Sex', 'Length', 'Diameter', 'Height', 'Wholeweight', 'Shuckedweight',
-                                            'Visceraweight', 'Shellweight'],
-                               'weights': coefs[i]})
-            print(fd)
-            print('intercept:{}\nalphas:{}\nmax(score):{}'.format(intercept[i], alphas[i], score[i]))
-    clf1 = linear_model.Ridge(fit_intercept=True)
-    clf1.set_params(alpha=c)
-    clf1.fit(X_train, y_train)
-    y_pre = clf1.predict(X_test)
-    sum_mean = 0
-    for i in range(len(y_pre)):
-        sum_mean += (y_pre[i] - y_test.values[i]) ** 2
-    sum_erro = np.sqrt(sum_mean / len(y_pre))
-    print('The value of RMSE in max(score):', sum_erro)
-    print('***************************************************')
-    # 浜ゅ弶楠岃瘉
-    clf2 = linear_model.RidgeCV(alphas=alphas)
-    clf2.fit(X_train, y_train)
-    fr = pd.DataFrame({'variable': ['Sex', 'Length', 'Diameter', 'Height', 'Wholeweight', 'Shuckedweight',
-                                    'Visceraweight', 'Shellweight'],
-                       'weights': clf2.coef_})
-    print(fr)
-    print('intercept:{}\nalphas:{}'.format(clf2.intercept_, clf2.alpha_))
-    y_pred = clf2.predict(X_test)
-        sum_mean = 0
-    for i in range(len(y_pred)):
-        sum_mean += (y_pred[i] - y_test.values[i]) ** 2
-    sum_erro = np.sqrt(sum_mean / len(y_pred))
-    print('the value of RMSE in cross validation:', sum_erro)
+    x,y=read_data()
+    Xmat = np.mat(x)
+    Ymat = np.mat(y)
+    xTx = Xmat.T*Xmat
+    xTxt = xTx + np.eye(Xarr.shape[1])*len
+    if np.linalg.eig(xTxt) == 0.0:
+        return
+    weight = xTxt.I * Xmat.T * Ymat
+  
     return data @ weight
    pass
 
 def lasso(data):
-    n_alphas = 200
-    alphas = np.logspace(-5, 0, n_alphas)  
-    coefs = []
-    score = []
-    intercept = []
-    c = 0
-    las = Lasso(normalize=True)
-    for a in alphas:
-        las.set_params(alpha=a)
-        las.fit(X_train, y_train)
-        coefs.append(las.coef_)
-        m = las.score(X_test, y_test)
-        n = las.intercept_
-        score.append(m)
-        intercept.append(n)
-    for i in range(len(score)):
-        if score[i] == max(score):
-            c = alphas[i]
-            fd = pd.DataFrame({'variable': ['Sex', 'Length', 'Diameter', 'Height', 'Wholeweight', 'Shuckedweight',
-                                            'Visceraweight', 'Shellweight'],
-                               'weights': coefs[i]})
-            print(fd)
-            print('intercept:{}\nalphas:{}\nmax(score):{}'.format(intercept[i], alphas[i], score[i]))
-    las1 = Lasso(normalize=True)
-    las1.set_params(alpha=c)
-    las1.fit(X_train, y_train)
-    y_pre = las1.predict(X_test)
-        sum_mean = 0
-    for i in range(len(y_pre)):
-        sum_mean += (y_pre[i] - y_test.values[i]) ** 2
-    sum_erro = np.sqrt(sum_mean / len(y_pre))
-    print('The value of RMSE in max(score):', sum_erro)
-    print('***************************************************')
-        lasso_cv = LassoCV(alphas=alphas, normalize=True, max_iter=1000, cv=None)
-    lasso_cv.fit(X_train, y_train)
-    fr = pd.DataFrame({'variable': ['Sex', 'Length', 'Diameter', 'Height', 'Wholeweight', 'Shuckedweight',
-                                    'Visceraweight', 'Shellweight'],
-                       'weights': lasso_cv.coef_})
-    print(fr)
-    print('intercept:{}\nalphas:{}'.format(lasso_cv.intercept_, lasso_cv.alpha_))
-    y_pred = lasso_cv.predict(X_test)
-    sum_mean = 0
-    for i in range(len(y_pred)):
-        sum_mean += (y_pred[i] - y_test.values[i]) ** 2
-    sum_erro = np.sqrt(sum_mean / 1254)
-    print('The value of RMSE in cross validation:', sum_erro)
-    
+  
     return data @ weight
     pass
 
